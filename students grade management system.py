138b50fc5972c75c0234edddb0a31923ca4ef82d
Junat S'Imsuk <tanujkusmi1@gmail.com>
2024-12-11 16:24:08 +0530

    students grade management system.py

--- students grade management system.py
+++ students grade management system.py
@@ -1,52 +1,65 @@
-student_dicts = {}
-def add_items(name,grade):
-    student_dicts[name] = grade
-    print(f"{name} with grade {grade} has been successfully added")
-
-def update_item(name,grade):
-    if name in student_dicts:
-        student_dicts[name] = grade
-        print(f"{name}'s grade has been updated")
-
-def del_item(name):
-    del student_dicts[name]
-    print(f"{name} has been deleted")
-
-def view_item():
-    for name,grade in student_dicts.items():
-        print(name ,":", grade)
+import sys #sys module is imported to exit from the program using exit() function
+class Information: #Intializing class
+    def __init__(self): #Initializing constructor
+        self.student_dicts={} #Initializing instance varialble as empty dictionary
+        self.main() #When the object of this class will be created then the main method will automatically called
     
+    def main(self): #This method ask user for performing different functionality.
+        ask_usr = int(input("Enter \n1 : Add\n2 : Update\n3 : Delete\n4 : View\n5 : Exit\n:"))
+        if ask_usr == 1:
+            self.add_items()
+        
+        elif ask_usr == 2:
+            self.update_item()
+        
+        elif ask_usr == 3:
+            self.del_item()
+        
+        elif ask_usr == 4:
+            self.view_item()
+        
+        elif ask_usr == 5:
+            sys.exit()
+    
+        else:
+            print(f"{ask_usr} not in the option")
 
-while True:
-    ask_usr = int(input("Enter \n1 : Add\n2 : Update\n3 : Delete\n4 : View\n5 : Exit\n:"))
-    if ask_usr == 1:
+#Below it different methods are defined for adding , deleting , updating , viewing students name and grade.
+###############################################################################
+    def add_items(self):
         ask_name = input("Enter your name : ")
         ask_grade = int(input("Enter your grade : "))
-        add_items(ask_name,ask_grade)
-        # student_dicts[ask_name] = ask_grade
-    
-    elif ask_usr == 2:
+        self.student_dicts[ask_name] = ask_grade
+        print(f"{ask_name} with grade {ask_grade} has been successfully added")
+        self.main()
+
+    def update_item(self):
         ask_name = input("Enter your name : ")
         ask_grade = int(input("Enter grade to update : "))
-        if ask_name in student_dicts:
-            update_item(ask_name,ask_grade)
+        if ask_name in self.student_dicts:
+            self.student_dicts[ask_name] = ask_grade
 
         else:
             print(f"{ask_name} is not in the list")
-    
-    elif ask_usr == 3:
-        ask_name = input("Enter your name : ")
-        if ask_name in student_dicts:
-            del_item(ask_name)
-    
-    elif ask_usr == 4:
-        view_item()
-    
-    elif ask_usr == 5:
-        break
-    
-    else:
-        print(f"{ask_usr} not in the option")
+        self.main()
 
-print("Thank You :) ")
-        
+
+    def del_item(self):
+        ask_name = input("Enter your name : ")
+        if ask_name in self.student_dicts:
+            del self.student_dicts[ask_name]
+            print(f"{ask_name} has been deleted")
+        else:
+            print(f"There is no data for name {ask_name}")
+    
+        self.main()
+
+
+    def view_item(self):
+        for name,grade in self.student_dicts.items():
+            print(name ,":", grade)
+            
+        self.main()
+############################################################################
+
+jh = Information() #Object of the class is created. And this object has ability to access methods and variables of class.
